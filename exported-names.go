fc76e72c76d1776d69913e57b7813dc1bfa57803
rohitmj <rohit.maharjan619@gmail.com>
2025-02-07 22:53:18 +1100

    Add files via upload

---
+++ exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+	fmt.Println("Euler's number is:", math.E)
+}
---
+++ imports.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Square root of 16 is %g\n", math.Sqrt(16))
+}
